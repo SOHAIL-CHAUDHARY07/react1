0fc1d4f355b59bf62842c0f99b6d2676ffaaed74
SOHAIL-CHAUDHARY07 <ree25097@gmail.com>
2026-01-21 03:59:06 +0500

    going-to -deploy2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Newsapp from './Components/Newsapp.js';
-import Card from './Components/Card.js';
+
 
 
 function App() {
   return (
-    <div>
+    
       <Newsapp />
-      <Card />  
-    </div>
+  
+    
   );
 }
 
